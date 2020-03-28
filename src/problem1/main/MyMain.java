02fb3f8fbedb34a7666404f04aec0a703749a16f
shubhi1502 <55740126+shubhi1502@users.noreply.github.com>
2020-03-28 15:51:36 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,16 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+      public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(1);
+        obj.insert(10);
+        obj.insert(4);
+        obj.insert(6);
+        System.out.println("ALL Left Nodes");
+        obj.printLeftNode();
+        System.out.println("Count of Nodes not having left node");
+        obj.countNotLeft();
+    }
 }
