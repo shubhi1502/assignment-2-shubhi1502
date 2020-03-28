7c46648dc83cc859f9b5e4d94f4ae5f6a51520b8
shubhi1502 <55740126+shubhi1502@users.noreply.github.com>
2020-03-28 16:02:02 +0530

    Update Node.java

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -7,4 +7,22 @@
 package problem5.node;
 // to define node properties
 public class Node {
+      Student student;
+    Node next;
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
 }
